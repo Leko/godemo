e28d75edd15948e68a9ecd2ad1794db0995196b4
Leko <leko.noor@gmail.com>
2015-07-21 00:15:59 +0900

    feat(app): Add /ping route. Gin example code

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
-
 package main
 
-func main() {}
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	router.Run(":" + port())
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = defaultPort
+	}
+
+	return port
+}
