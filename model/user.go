b7ca55831b764c4bf410895517fc17b0f9512264
Leko <leko.noor@gmail.com>
2015-12-05 21:54:53 +0900

    feat: Generate and Display API token

--- model/user.go
+++ model/user.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/nu7hatch/gouuid"
 	"godemo/database"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -10,6 +11,7 @@ type User struct {
 	gorm.Model
 
 	Email    string `sql:"not null;unique_index"`
+	Token    string `sql:"not null"`
 	Password string `sql:"not null"`
 }
 
@@ -20,6 +22,14 @@ func PasswordHash(password string) string {
 	}
 
 	return string(hashed)
+}
+
+func (u *User) BeforeSave() {
+	token, err := getUuid(u.Email)
+	if err != nil {
+		panic(err)
+	}
+	u.Token = token
 }
 
 func (u *User) Auth() (int, error) {
@@ -34,4 +44,16 @@ func (u *User) Auth() (int, error) {
 	// https://godoc.org/golang.org/x/crypto/bcrypt#CompareHashAndPassword
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	return int(user.ID), err
+}
+
+func getUuid(signature string) (string, error) {
+	var uid string
+	u5, err := uuid.NewV5(uuid.NamespaceURL, []byte(signature))
+	if err == nil {
+		uid = u5.String()
+	} else {
+		uid = ""
+	}
+
+	return uid, err
 }
